c72a23e678b23750cf740a77512186b792c4b3cf
i-am-harveyt <haotingtong30@gmail.com>
2023-03-22 08:54:06 +0800

    Add keybindings to nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,12 +3,36 @@ if not ok then
   print("Nvim-Tree not installed")
   return
 end
-
+nvt.setup({
+	update_focused_file = {
+		enable = true,
+	},
+	view = {
+		mappings = {
+			list = {
+				{ key = 'c', action = "cd" },
+				{ key = 'N', action = "create" },
+				{ key = 'u', action = "dir_up" },
+				{ key = 't', action = "tabnew" },
+				{ key = '?', action = "toggle_help" },
+			}
+		}
+	},
+	git = {
+		enable = true,
+		show_on_dirs = true,
+		timeout = 200,
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+	}
+})
 
 local wk_ok, wk = pcall(require, "which-key")
 if not wk_ok then
 	print("Which-key not installed")
-	return packer
+	return
 end
 wk.register({
   e = {"<Cmd>NvimTreeToggle<CR>", "Explorer"},
@@ -20,4 +44,3 @@ wk.register({
 }, { prefix = "<Space>" })
 
 
-nvt.setup()
