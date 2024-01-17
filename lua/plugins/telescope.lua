cadc7feae22f36f45219b1b85135a99efd4b5b81
i-am-harveyt <haotingtong30@gmail.com>
2024-01-17 14:23:25 +0800

    Add: <CR>s

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,8 @@ local config = function()
 	require("telescope").setup()
 end
 
-vim.keymap.set("n", "<C-f>w", ":Telescope live_grep", { desc = "Find Words", silent = true })
-vim.keymap.set("n", "<C-f>f", ":Telescope find_files", { desc = "Find Files", silent = true })
+vim.keymap.set("n", "<C-f>w", ":Telescope live_grep<CR>", { desc = "Find Words", silent = true })
+vim.keymap.set("n", "<C-f>f", ":Telescope find_files<CR>", { desc = "Find Files", silent = true })
 
 return {
 	"nvim-telescope/telescope.nvim",
